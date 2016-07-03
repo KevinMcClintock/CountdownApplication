06e7aacc09d740edcf16b033c0f38494ed24d9ff
KevinMcClintock <kevin_mcclintock@live.com>
2016-07-03 14:58:08 +1000

    Added configuration

--- app/src/main/java/com/example/kmcclintock/countdown_Widget/Configure.java
+++ app/src/main/java/com/example/kmcclintock/countdown_Widget/Configure.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by KMcClintock on 26/06/2016.
@@ -18,6 +24,7 @@ public class Configure extends Activity {
 
     private Configure context;
     private int widgetID;
+    GetDateTime.dateTime dt = new GetDateTime.dateTime();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +41,25 @@ public class Configure extends Activity {
         final AppWidgetManager widgetManager = AppWidgetManager.getInstance(context);
         final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_main);
 
+        DatePicker dp = (DatePicker)findViewById(R.id.datePicker);
         Button b = (Button)findViewById(R.id.launch);
+        //final TextView tv = (TextView)findViewById(R.id.textView);
+        final Calendar today = Calendar.getInstance();
+
+        //Set all the date information
+        dp.init(today.get(Calendar.YEAR)
+                ,today.get(Calendar.MONTH)
+                ,today.get(Calendar.DAY_OF_MONTH),
+        new DatePicker.OnDateChangedListener() {
+
+            @Override
+            public void onDateChanged(DatePicker view,
+                                      int year, int monthOfYear,int dayOfMonth) {
+                dt.setYear(year);
+                dt.setMonth(monthOfYear);
+                dt.setDay(dayOfMonth);
+            }});
+
         b.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -50,8 +75,17 @@ public class Configure extends Activity {
 
                 Intent resultValue = new Intent();
                 resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,widgetID);
+
                 //Set the return result
                 setResult(RESULT_OK,resultValue);
+
+                DateFormat format = SimpleDateFormat.getTimeInstance(
+                SimpleDateFormat.MEDIUM, Locale.getDefault());
+                int dateTime = dt.getDay() + dt.getMonth() + dt.getYear();
+                CharSequence dateTimeFormat = format.format(dateTime);
+
+                views.setTextViewText(R.id.textView,dateTimeFormat);
+
 
                 //Close Activity
                 finish();
--- app/src/main/java/com/example/kmcclintock/countdown_Widget/MainActivity.java
+++ app/src/main/java/com/example/kmcclintock/countdown_Widget/MainActivity.java
@@ -7,24 +7,43 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppWidgetProvider {
 
+    //GetDateTime.dateTime dt = new GetDateTime.dateTime();
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        //DateFormat format = SimpleDateFormat.getTimeInstance(
+                //SimpleDateFormat.MEDIUM, Locale.getDefault());
+        //int dateTime = dt.getDay() + dt.getMonth() + dt.getYear();
+       // CharSequence dateTimeFormat = format.format(dateTime);
+
         for (int i = 0; i < appWidgetIds.length; i++) {
             int currentWidgetId = appWidgetIds[i];
             String url = "http://www.tutorialspoint.com";
+
 
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.setData(Uri.parse(url));
 
+
+
             PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_main);
+            //views.setTextViewText(R.id.textView,dateTimeFormat);
+
+
 
             views.setOnClickPendingIntent(R.id.button, pending);
             appWidgetManager.updateAppWidget(currentWidgetId, views);
