aef14f880ba3bbf68e71f170cdacafb03eee06c7
KevinMcClintock <kevin_mcclintock@live.com>
2016-07-07 19:30:34 +1000

    getters setters

---
+++ app/src/main/java/com/example/kmcclintock/countdown_Widget/GetDateTime.java
@@ -0,0 +1,43 @@
+package com.example.kmcclintock.countdown_Widget;
+
+/**
+ * Created by KMcClintock on 28/06/2016.
+ */
+public class GetDateTime {
+
+
+static class dateTime{
+
+    private int day;
+    private int month;
+    private int year;
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
+
+
+
+
+}
